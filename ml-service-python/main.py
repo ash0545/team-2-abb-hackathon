a0fcad7a3c02587e4c36cb9ec9eec18e498352d5
ashwin.santhosh2004 <112403369+ash0545@users.noreply.github.com>
2025-08-04 10:48:44 +0000

    feat: initialize FastAPI backend

---
+++ ml-service-python/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "ML Service is alive"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
